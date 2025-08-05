c7b723a3fd39ce76950546496a3442b5055a43e3
Michael <mredw601@learner.dvusd.org>
2025-08-05 08:12:04 -0700

    student questions

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Linq;
+
 Console.WriteLine("Hello, World!");
+string first;
+string last;
+string grade;
+string id;
+Console.WriteLine("what is your first name");
+first = Console.ReadLine($"nice to meet you {first}");
+Console.WriteLine("what is your last name");
+last = Console.ReadLine($"cool last name {last} ");
+Console.WriteLine("what grade are in");
+grade = Console.ReadLine($"nice grade {grade}");
+Console.WriteLine("what is your  id");
+id = Console.ReadLine($"get scammed {id}");
